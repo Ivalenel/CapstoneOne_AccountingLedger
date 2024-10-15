ff1778b7efcf741bac105b018f30b9f5e7d8a700
Ivalene <ivalenelarrea234@gmail.com>
2024-10-15 09:31:30 -0500

    begining menu

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,26 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner commnandScanner = new Scanner(System.in);
+    static Scanner inputScanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     int mainMenuCommand;
+
+     ///***** Home ******
+        //        //Add deposit
+        //        //Make Payment
+        //        //Lerger
+        //        //Exit
+        //        //Leger
+     do{
+         System.out.println();
+         System.out.println();
+         System.out.println();
+
+
+        }
+
     }
 }
